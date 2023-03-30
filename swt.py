angle calculation fixed

swt.py
@@ -3,11 +3,12 @@ from typing import Iterator, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
+from skimage import measure
 from skimage.draw import line as skline
 from skimage.feature import canny
 from skimage.filters import scharr_h, scharr_v
 from skimage.io import imread
-from skimage.measure import label
+from swt_src import swt as swt2
 
 Image = np.ndarray
 SubImage = np.ndarray
@@ -19,14 +20,18 @@ Gradient = Tuple[float, float]
 Ray = Iterator[Tuple[int, int]]
 
 
-def project_ray(diag: float, start: Position, gradient: Gradient, grad_dir: int) -> Ray:
-    x_grad, y_grad = gradient
-    # Calculate the angle of the gradient
-    angle = np.arctan2(y_grad * grad_dir, x_grad * grad_dir)
+def derivatives(img: Image, grad_dir: int) -> Tuple[Image, Gradients, Image]:
+    # edge_map and x, y gradients and angles
+    gradients = (scharr_h(img), scharr_v(img))
+    angles = np.arctan2(gradients[1] * grad_dir, gradients[0] * grad_dir)
+    return canny(img), gradients, angles
 
+
+def project_ray(diag: float, start: Position, angles: Image) -> Ray:
     # Calculate the end point of the line
-    end_x = floor(start[0] + diag * np.cos(angle))
-    end_y = floor(start[1] + diag * np.sin(angle))
+    end_x = floor(start[0] + diag * np.cos(angles[start]))
+    end_y = floor(start[1] + diag * np.sin(angles[start]))
+    skline(start[0], start[1], end_x, end_y)
 
     return zip(*skline(start[0], start[1], end_x, end_y))
 
@@ -51,43 +56,34 @@ def traverse_line(line: Ray, dims: Dimensions, edge_map: Image) -> Position:
     return end
 
 
-def angle_diff(gradient, gradients: Gradients, end: Position) -> bool:
-    # calculate the angle at both points
-    startAngle = np.arctan2(gradient[1], gradient[0])
-    endAngle = np.arctan2(gradients[1][end], gradients[0][end])
-
-    # calculate the difference between the angles
-    angle = np.abs(startAngle - (endAngle + np.pi) % np.pi)
-    return angle > np.pi / 6
-
-
 def plot_new_line(img: Image, start: Position, end: Position):
     # calculate the distance between the edge point and the endpoint
     dist = np.sqrt((start[0] - end[0]) ** 2 + (start[1] - end[1]) ** 2)
     line = skline(start[0], start[1], end[0], end[1])
 
-    # a pixel is equal to the shortest stroke it is part of
-    for idx in zip(*line):
-        if (img[idx] > dist) or (img[idx] == np.Infinity):
-            img[idx] = dist
+    # Update pixels that belong to the new line with the minimum distance
+    img[line] = np.minimum(img[line], dist)
 
     return line
 
 
 def set_ray_above_median(ray: Ray, image: Image):
-    # get pixel values for each pixel in the ray
-    pixel_values = [image[pixel] for pixel in zip(*ray)]
-
-    # compute the median pixel value
-    median_value = np.median(pixel_values)
+    median_value = np.median(image[ray])
 
     # set all pixels above the median to the median value
-    for pixel in zip(*ray):
-        if image[pixel] >= median_value:
-            image[pixel] = median_value
+    image[ray][image[ray] >= median_value] = median_value
 
 
-def swt(img: Image, edge_map: Image, gradients: Gradients, grad_dir: int) -> Image:
+def angle_between(start, end, gradients):
+    v1 = (gradients[0][start], gradients[1][start])
+    v2 = (gradients[0][end], gradients[1][end])
+
+    dotmag = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+    angle = np.abs(np.arccos(np.clip(dotmag, -1, 1)) - np.pi)
+    return angle
+
+
+def swt(img: Image, edge_map: Image, gradients: Gradients, angles: Image) -> Image:
     # Output image
     projection = np.ones(img.shape) * np.Infinity
 
@@ -104,11 +100,8 @@ def swt(img: Image, edge_map: Image, gradients: Gradients, grad_dir: int) -> Ima
     for edge_x, edge_y in np.argwhere(edge_map == 1):
         start = edge_x, edge_y
 
-        # x and y gradient at current point
-        gradient = (gradients[0][start], gradients[1][start])
-
         # project a line from the edge point in the direction of the gradient
-        ray = project_ray(diaglen, start, gradient, grad_dir)
+        ray = project_ray(diaglen, start, angles)
 
         # track if endpoint is found
         end = traverse_line(ray, dims, edge_map)
@@ -116,8 +109,7 @@ def swt(img: Image, edge_map: Image, gradients: Gradients, grad_dir: int) -> Ima
         if end is None:
             continue
 
-        # a difference of more than pi/6 is not a valid edge
-        if angle_diff(gradient, gradients, end):
+        if angle_between(start, end, gradients) > np.pi / 6:
             continue
 
         # plot the new line
@@ -133,7 +125,7 @@ def swt(img: Image, edge_map: Image, gradients: Gradients, grad_dir: int) -> Ima
     return projection
 
 
-def store_equivalent_labels(eq_labels, label1, label2):
+def store_equivalent_labels(eq_labels: dict, label1, label2):
     """
     Store the equivalence relationship between two labels in a dictionary.
 
@@ -158,6 +150,7 @@ def store_equivalent_labels(eq_labels, label1, label2):
 def connected_components(swt: Image):
     label = 0
     labels = np.zeros(swt.shape, dtype=np.int32)
+    eq_labels = {}
 
     def neighbors(i, j):
         yield i - 1, j
@@ -165,41 +158,126 @@ def connected_components(swt: Image):
         yield i - 1, j - 1
 
     for i, j in np.argwhere(swt != 0):
-        neighs = [labels[x, y] for x, y in neighbors(i, j) if labels[x, y] != 0]
-        neighs = list(set(neighs))
+
+        def keep(x1, y1):
+            max_ratio = 2
+            ratio = np.abs(swt[x1, y1] / swt[i, j])
+            ratio = ratio > 1 / max_ratio and ratio < max_ratio
+            return (labels[x1, y1] != 0) and ratio
+
+        neighs = [labels[x, y] for x, y in neighbors(i, j) if keep(x, y)]
+        neighs = sorted(list(set(neighs)))
 
         if not neighs:
             labels[i, j] = label = label + 1
             continue
 
-        labels[i, j] = min(neighs)
+        labels[i, j] = neighs[0]
+
+        for n in neighs[1:]:
+            store_equivalent_labels(eq_labels, neighs[0], n)
+
+    for i, j in np.argwhere(labels != 0):
+        root = labels[i, j]
+        while eq_labels.get(root, None) is not None:
+            root = eq_labels[root]
+        labels[i, j] = root
+
+    reduced_labels = sorted(list(set(labels.ravel())))
+    for i, label in enumerate(reduced_labels):
+        labels[labels == label] = i
 
     return labels
 
 
-if __name__ == "__main__":
-    fig, axes = plt.subplots(2, 1, figsize=(8, 8))
+def connected(swt: Image):
+    def is_connected(p1, p2):
+        return p1 != 0 and p2 != 0 and abs(p1 / p2) < 3 and abs(p2 / p1) < 3
+
+    # Label connected components using scikit-image
+    labeled_image = measure.label(swt, connectivity=2, background=0)
+
+    # Iterate over all labeled regions and check their connectivity
+    for region in measure.regionprops(labeled_image, intensity_image=swt):
+        region_intensity = region.mean_intensity
+        for coord in region.coords:
+            pixel_intensity = swt[coord[0], coord[1]]
+            if not is_connected(region_intensity, pixel_intensity):
+                labeled_image[coord[0], coord[1]] = 0
+
+    return labeled_image
+
+
+def compare():
+    fig, axes = plt.subplots(2, 3, figsize=(8, 8))
     ax = axes.ravel()
 
-    img = imread("../Images/test/text-0.png", as_gray=True)
-    # declare a new image type
+    # img = imread("../Images/test/text-0.png", as_gray=True)
+    img = imread("../Images/test/text.jpg", as_gray=True)
+    # img = imread("../Images/test/swt-test.png", as_gray=True)
 
+    edges, gradients, angles = derivatives(img, -1)
+    swt_img = swt(img, edges, gradients, angles)
+
+    edges2, angles2, swt_img2 = swt2(img)
+
+    ax[0].set_title("SK Edge Map")
+    ax[0].imshow(edges, cmap="gray")
+
+    ax[1].set_title("Gradient Map")
+    ax[1].imshow(angles * 180 / np.pi, cmap="gray")
+
+    ax[2].set_title("My SWT")
+    ax[2].imshow(swt_img, cmap="gray")
+
+    ax[3].set_title("CV Edge Map")
+    ax[3].imshow(edges2, cmap="gray")
+
+    ax[4].set_title("Gradient Map")
+    ax[4].imshow(angles2 * 180 / np.pi, cmap="gray")
+
+    ax[5].set_title("OG SWT")
+    ax[5].imshow(swt_img2, cmap="gray")
+
+    plt.show()
+
+
+def compare2():
+    fig, axes = plt.subplots(2, 1, figsize=(8, 8))
+    ax = axes.ravel()
+    img = imread("../Images/test/swt-test.png", as_gray=True)
     # img = imread("../Images/test/text.jpg", as_gray=True)
 
-    canny_img = canny(img)
-    gradients = (scharr_h(img), scharr_v(img))
-    swt_img = swt(img, canny_img, gradients, -1)
-    # con_img = label(
-    #     swt_img,
-    #     # img
-    #     connectivity=2,
-    #     # return_num=False,
-    # )
-    con_img = connected_components(swt_img)
+    edges, gradients, angles = derivatives(img, -1)
+    swt_img = swt(img, edges, gradients, angles)
+    edges, gradients, angles = derivatives(img, 1)
+    swt_img2 = swt(img, edges, gradients, angles)
 
     ax[0].set_title("SWT")
     ax[0].imshow(swt_img, cmap="gray")
 
-    ax[1].set_title("Connected Components")
-    ax[1].imshow(con_img, cmap="gray")
+    ax[1].set_title("SWT")
+    ax[1].imshow(swt_img2, cmap="gray")
+
     plt.show()
+
+
+def test():
+    fig, axes = plt.subplots(1, 1, figsize=(8, 8))
+
+    img = imread("../Images/test/text-0.png", as_gray=True)
+    # img = imread("../Images/test/text.jpg", as_gray=True)
+
+    edges, gradients, angles = derivatives(img, -1)
+    swt_img = swt(img, edges, gradients, angles)
+
+    axes.set_title("SWT")
+    axes.imshow(swt_img, cmap="gray")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    compare()
+    # compare2()
+    # test()