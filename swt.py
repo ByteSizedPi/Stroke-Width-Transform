54f45dba50c9cf491574d7fbfaf6189ab78a8885
Johan Venter <johann12venter@gmail.com>
2023-03-31 10:42:37 +0200

    connected components function working

--- swt.py
+++ swt.py
@@ -70,14 +70,16 @@ def plot_new_line(img: Image, start: Position, end: Position):
 def set_ray_above_median(ray: Ray, image: Image):
     median_value = np.median(image[ray])
 
-    # set all pixels above the median to the median value
-    image[ray][image[ray] >= median_value] = median_value
+    for r, c in zip(*ray):
+        if image[r, c] >= median_value:
+            image[r, c] = median_value
 
 
-def angle_between(start, end, gradients):
+def angle_between(start: Position, end: Position, gradients: Gradients) -> float:
     v1 = (gradients[0][start], gradients[1][start])
     v2 = (gradients[0][end], gradients[1][end])
 
+    # determine angle by using the formula for the dot product
     dotmag = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
     angle = np.abs(np.arccos(np.clip(dotmag, -1, 1)) - np.pi)
     return angle
@@ -109,6 +111,7 @@ def swt(img: Image, edge_map: Image, gradients: Gradients, angles: Image) -> Ima
         if end is None:
             continue
 
+        # lines that are not roughlly opposite are discarded
         if angle_between(start, end, gradients) > np.pi / 6:
             continue
 
@@ -147,65 +150,72 @@ def store_equivalent_labels(eq_labels: dict, label1, label2):
         eq_labels[root2] = root1
 
 
-def connected_components(swt: Image):
+def simplify_labels(labels: dict):
+    unique = np.unique(labels)
+
+    # create an array of successive integer labels
+    new_labels = np.arange(len(unique))
+
+    # create a dictionary to map old labels to new labels
+    label_map = dict(zip(unique, new_labels))
+
+    # replace the old labels with new labels
+    for old_label, new_label in label_map.items():
+        labels[labels == old_label] = new_label
+
+
+def connected_components(swt: Image) -> Image:
     label = 0
     labels = np.zeros(swt.shape, dtype=np.int32)
     eq_labels = {}
 
-    def neighbors(i, j):
-        yield i - 1, j
-        yield i, j - 1
-        yield i - 1, j - 1
+    def valid_neighbors(idx):
+        # neighbors iterator for 8-connectivity
+        def neighbors(idx):
+            i, j = idx
+            yield i - 1, j
+            yield i, j - 1
+            yield i - 1, j - 1
 
-    for i, j in np.argwhere(swt != 0):
+        # valid neighbors have a ratio < 2 relative to current pixel
+        def is_valid(p, idx, max_ratio=3):
+            ratio = np.abs(swt[p] / swt[idx])
+            return ratio < max_ratio and ratio > 1 / max_ratio
 
-        def keep(x1, y1):
-            max_ratio = 2
-            ratio = np.abs(swt[x1, y1] / swt[i, j])
-            ratio = ratio > 1 / max_ratio and ratio < max_ratio
-            return (labels[x1, y1] != 0) and ratio
+        # return an array of indices of valid neighbors
+        return [p for p in neighbors(idx) if is_valid(p, idx)]
 
-        neighs = [labels[x, y] for x, y in neighbors(i, j) if keep(x, y)]
-        neighs = sorted(list(set(neighs)))
+    for point in np.argwhere(swt != 0):
+        i, j = point
 
+        # get the label values of the valid neighbors of the current point
+        neighs = [labels[n] for n in valid_neighbors((i, j))]
+
+        # sorted unique neighbors
+        neighs = sorted(set(neighs))
+
+        # if no neighbors, create a new label
         if not neighs:
-            labels[i, j] = label = label + 1
+            label += 1
+            labels[i, j] = label
             continue
 
+        # if one or more neighbor, assign the current point that label
         labels[i, j] = neighs[0]
 
+        # store all labels as equivalent
         for n in neighs[1:]:
             store_equivalent_labels(eq_labels, neighs[0], n)
 
+    # replace all labels with their root labels
     for i, j in np.argwhere(labels != 0):
         root = labels[i, j]
         while eq_labels.get(root, None) is not None:
             root = eq_labels[root]
         labels[i, j] = root
 
-    reduced_labels = sorted(list(set(labels.ravel())))
-    for i, label in enumerate(reduced_labels):
-        labels[labels == label] = i
-
+    simplify_labels(labels)
     return labels
-
-
-def connected(swt: Image):
-    def is_connected(p1, p2):
-        return p1 != 0 and p2 != 0 and abs(p1 / p2) < 3 and abs(p2 / p1) < 3
-
-    # Label connected components using scikit-image
-    labeled_image = measure.label(swt, connectivity=2, background=0)
-
-    # Iterate over all labeled regions and check their connectivity
-    for region in measure.regionprops(labeled_image, intensity_image=swt):
-        region_intensity = region.mean_intensity
-        for coord in region.coords:
-            pixel_intensity = swt[coord[0], coord[1]]
-            if not is_connected(region_intensity, pixel_intensity):
-                labeled_image[coord[0], coord[1]] = 0
-
-    return labeled_image
 
 
 def compare():
@@ -213,8 +223,8 @@ def compare():
     ax = axes.ravel()
 
     # img = imread("../Images/test/text-0.png", as_gray=True)
-    img = imread("../Images/test/text.jpg", as_gray=True)
-    # img = imread("../Images/test/swt-test.png", as_gray=True)
+    # img = imread("../Images/test/text.jpg", as_gray=True)
+    img = imread("../Images/test/swt-test.png", as_gray=True)
 
     edges, gradients, angles = derivatives(img, -1)
     swt_img = swt(img, edges, gradients, angles)
@@ -250,8 +260,8 @@ def compare2():
 
     edges, gradients, angles = derivatives(img, -1)
     swt_img = swt(img, edges, gradients, angles)
-    edges, gradients, angles = derivatives(img, 1)
-    swt_img2 = swt(img, edges, gradients, angles)
+    # edges, gradients, angles = derivatives(img, 1)
+    swt_img2 = connected_components(swt_img)
 
     ax[0].set_title("SWT")
     ax[0].imshow(swt_img, cmap="gray")
@@ -265,19 +275,21 @@ def compare2():
 def test():
     fig, axes = plt.subplots(1, 1, figsize=(8, 8))
 
-    img = imread("../Images/test/text-0.png", as_gray=True)
-    # img = imread("../Images/test/text.jpg", as_gray=True)
+    # img = imread("../Images/test/swt-test.png", as_gray=True)
+    img = imread("../Images/test/text.jpg", as_gray=True)
 
     edges, gradients, angles = derivatives(img, -1)
     swt_img = swt(img, edges, gradients, angles)
+    connected = connected_components(swt_img)
 
     axes.set_title("SWT")
-    axes.imshow(swt_img, cmap="gray")
+    axes.imshow(connected)
+    # axes.imshow(connected, cmap="gray")
 
     plt.show()
 
 
 if __name__ == "__main__":
-    compare()
+    # compare()
     # compare2()
-    # test()
+    test()
