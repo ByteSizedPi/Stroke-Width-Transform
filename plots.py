9093b95251980125a10900f69dda89ca8699bd4f
Johan Venter <johann12venter@gmail.com>
2024-02-01 12:35:50 +0200

    initial commit

---
+++ plots.py
@@ -0,0 +1,31 @@
+from math import ceil, sqrt
+
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = (15, 10)
+
+
+def dims(i):
+    c = ceil(sqrt(i))
+    r = ceil(i / c)
+    return r, c
+
+
+def plot(images, cmap="gray", titles=[]):
+    r, c = dims(len(images))
+    _, ax = plt.subplots(r, c)
+
+    if len(images) > 1:
+        ax = ax.ravel()
+        for i, img in enumerate(images):
+            ax[i].imshow(img, cmap=cmap)
+            ax[i].set_axis_off()
+            if len(titles) > 0:
+                ax[i].set_title(titles[i])
+    else:
+        ax.imshow(images[0], cmap=cmap)
+        ax.set_axis_off()
+        if len(titles) > 0:
+            ax.set_title(titles[0])
+    plt.tight_layout()
+    plt.show()
